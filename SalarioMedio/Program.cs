4b0e7517d2dd1d7fc71adb7c5b71cdcdec79d820
axcel0773 <yuji-watanabe1@hotmail.com>
2021-10-16 15:55:24 -0300

    Adicionar arquivos de projeto.

---
+++ SalarioMedio/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SalarioMedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salarioFuncionario;
+            double salarioMinimo;
+            double resultado;
+
+            Console.WriteLine("Digite o valor do seu salário:");
+            salarioFuncionario = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do salário mínimo:");
+            salarioMinimo = Convert.ToDouble(Console.ReadLine());
+
+            resultado = (salarioFuncionario / salarioMinimo);
+
+            Console.WriteLine("Você recebe {0} salário mínimo", resultado);
+        }
+    }
+}
